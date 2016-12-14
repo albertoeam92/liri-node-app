e76df0455ccb98171be1933aa4a316e30b726f1f
Alberto Alvarez <albertoealvarezm1992@gmail.com>
2016-12-14 00:57:23 -0500

    omdb request function created

--- liri.js
+++ liri.js
@@ -1,10 +1,12 @@
 
 var fs = require('fs');
 var apiKeys = require('./keys.js');
-
+var request = require('request');
 
 var command = process.argv[2];
 var userinput = process.argv[3];
+console.log(userinput);
+
 
 console.log('-----------');
 console.log('Command: ', command);
@@ -33,16 +35,60 @@ switch(command) {
 			break;
 }
 
-function TwitterRequest (){
-	console.log('Twitter API keys: ', apiKeys.twitterKeys);
+function TwitterRequest (){  
+
 };
 
 function SpotifyRequest(){
-	console.log('Spotify API keys: ', apiKeys.spotifyKeys);
+
 };
 
 function OmdbRequest (){
-	console.log('Omdb API key: ', apiKeys.omdbKeys.api_key);
+
+
+	if (userinput == undefined){
+		request('http://www.omdbapi.com/?t=mr+nobody&tomatoes=true&r=json', function(error,response,body){
+			
+			if (!error && response.statusCode === 200) {
+
+				var movie = JSON.parse(body);
+
+				console.log('\nMovie: '+ movie.Title+ 
+			    	'\nReleased: ' + movie.Released + 
+			    	'\nIMDB rating: '+ movie.imdbRating + 
+			    	'\nCountry: ' + movie.Country + 
+			    	'\nLanguage: ' + movie.Language + 
+			    	'\nPlot: ' + movie.Plot + 
+			    	'\nActors: ' + movie.Actors + 
+			    	'\nRotten Tomatoes rating: ' + movie.tomatoRating + 
+			    	'\nRotten Tomatoes page: ' + movie.tomatoURL +
+			    	'\n-----------');
+			};
+
+		});
+
+	} else {
+		request('http://www.omdbapi.com/?t=' + userinput + '&tomatoes=true&r=json', function(error,response,body){
+
+			if (!error && response.statusCode === 200){
+
+				var movie = JSON.parse(body);
+
+			    console.log('\nMovie: '+ movie.Title+ 
+			    	'\nReleased: ' + movie.Released + 
+			    	'\nIMDB rating: '+ movie.imdbRating + 
+			    	'\nCountry: ' + movie.Country + 
+			    	'\nLanguage: ' + movie.Language + 
+			    	'\nPlot: ' + movie.Plot + 
+			    	'\nActors: ' + movie.Actors + 
+			    	'\nRotten Tomatoes rating: ' + movie.tomatoRating + 
+			    	'\nRotten Tomatoes page: ' + movie.tomatoURL +
+			    	'\n-----------');
+			};
+	    });
+	  }
+
+	
 };
 
 function LiriBot(){
